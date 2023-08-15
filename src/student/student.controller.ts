936d6927f5acc095c47a0ce359ee2fa07382db3b
S1riyS <kirill.ankudinov.94@mail.ru>
2023-08-15 22:50:00 +0500

    feat(student): added serialization of responses

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,23 +1,44 @@
-import { Body, Controller, Get, Param, ParseUUIDPipe, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  ClassSerializerInterceptor,
+  Controller,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Put,
+  UseInterceptors,
+} from '@nestjs/common';
 import { StudentService } from './student.service';
 import { CreateStudentProfileDTO, UpdateStudentProfileDTO } from './dto';
+import { ProfileResponse } from './responses';
 
 @Controller('students')
+@UseInterceptors(ClassSerializerInterceptor)
 export class StudentController {
   constructor(private readonly studentService: StudentService) {}
 
   @Get(':userId/profile')
-  findOne(@Param('userId', ParseUUIDPipe) userId: string) {
-    return this.studentService.findOneProfile(userId);
+  async findOne(@Param('userId', ParseUUIDPipe) userId: string) {
+    const profile = await this.studentService.findOneProfile(userId);
+    return new ProfileResponse(profile);
   }
 
   @Post(':userId/profile')
-  createProfile(@Param('userId', ParseUUIDPipe) userId: string, @Body() createStudentDto: CreateStudentProfileDTO) {
-    return this.studentService.createProfile(userId, createStudentDto);
+  async createProfile(
+    @Param('userId', ParseUUIDPipe) userId: string,
+    @Body() createStudentDto: CreateStudentProfileDTO,
+  ) {
+    const profile = await this.studentService.createProfile(userId, createStudentDto);
+    return new ProfileResponse(profile);
   }
 
   @Put(':userId/profile')
-  updateProfile(@Param('userId', ParseUUIDPipe) userId: string, @Body() updateStudentDto: UpdateStudentProfileDTO) {
-    return this.studentService.updateProfile(userId, updateStudentDto);
+  async updateProfile(
+    @Param('userId', ParseUUIDPipe) userId: string,
+    @Body() updateStudentDto: UpdateStudentProfileDTO,
+  ) {
+    const profile = await this.studentService.updateProfile(userId, updateStudentDto);
+    return new ProfileResponse(profile);
   }
 }
