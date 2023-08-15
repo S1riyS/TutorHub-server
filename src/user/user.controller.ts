9c5199c714e94eed66878606f3e719d20beb19ce
S1riyS <kirill.ankudinov.94@mail.ru>
2023-08-15 23:53:22 +0500

    feat(user): responses are serialized

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,19 +1,33 @@
-import { Body, Controller, Delete, Get, Param, ParseUUIDPipe, Put } from '@nestjs/common';
+import {
+  Body,
+  ClassSerializerInterceptor,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Put,
+  UseInterceptors,
+} from '@nestjs/common';
 import { UserService } from '@user/user.service';
 import { UpdateUserDTO } from '@user/dto';
+import { UserResponse } from '@user/responses';
 
 @Controller('users')
+@UseInterceptors(ClassSerializerInterceptor)
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get(':id')
   async findOne(@Param('id', ParseUUIDPipe) id: string) {
-    return this.userService.findOne(id);
+    const user = await this.userService.findOne(id);
+    return new UserResponse(user);
   }
 
   @Put(':id')
   async update(@Param('id', ParseUUIDPipe) id: string, @Body() dto: UpdateUserDTO) {
-    return this.userService.update(id, dto);
+    const user = await this.userService.update(id, dto);
+    return new UserResponse(user);
   }
 
   @Delete(':id')
