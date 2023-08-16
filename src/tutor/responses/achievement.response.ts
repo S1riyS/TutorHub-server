2fda7fb957fd26ed7f41edf25de913bffa2cb33e
S1riyS <kirill.ankudinov.94@mail.ru>
2023-08-16 21:11:05 +0500

    fix(tutor): added missed constructor method in AchievementEntity

--- src/tutor/responses/achievement.response.ts
+++ src/tutor/responses/achievement.response.ts
@@ -1,3 +1,8 @@
 import { AchievementEntity } from '../entities';
 
-export class AchievementResponse extends AchievementEntity {}
+export class AchievementResponse extends AchievementEntity {
+  constructor(achievement: Partial<AchievementEntity>) {
+    super();
+    Object.assign(this, achievement);
+  }
+}
