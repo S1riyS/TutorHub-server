a669ba00bd47a2a5f59011c8264baeb45465bb68
S1riyS <kirill.ankudinov.94@mail.ru>
2023-08-16 14:17:10 +0500

    feat(app): `swaggerSetup` is called on app creation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import swaggerSetup from './swaggerSetup';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,9 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+
+  await swaggerSetup(app);
+
   await app.listen(3000);
 }
 
