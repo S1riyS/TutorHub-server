c8689b4169db9b869a8fc9b9d53ca14101269dfe
S1riyS <kirill.ankudinov.94@mail.ru>
2023-08-16 16:16:00 +0500

    feat(student): DTOs and responses are documented

--- src/student/dto/profile.dto.ts
+++ src/student/dto/profile.dto.ts
@@ -1,9 +1,10 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber } from 'class-validator';
 
 export class CreateStudentProfileDTO {
   @IsNotEmpty()
   @IsNumber()
+  @ApiProperty({ example: 10 })
   grade: number;
 }
 
