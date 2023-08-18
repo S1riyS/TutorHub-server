5ce80fd97c61e6b72deb1b6dabd281aaa577fd1b
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2023-08-18 13:56:02 +0500

    feat(config): added global ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,19 @@ import { PrismaModule } from '@prisma/prisma.module';
 import { UserModule } from '@user/user.module';
 import { TutorModule } from './tutor/tutor.module';
 import { StudentModule } from './student/student.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule, UserModule, TutorModule, StudentModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    PrismaModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    UserModule,
+    TutorModule,
+    StudentModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
