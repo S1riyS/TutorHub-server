405dfe5364f16d261bae2d9875e6a4b68a7c7a9c
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2023-08-19 15:32:01 +0500

    feat(auth): `JwtAuthGuard` is global

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { TutorModule } from './tutor/tutor.module';
 import { StudentModule } from './student/student.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 
 @Module({
   imports: [
@@ -16,6 +18,12 @@ import { ConfigModule } from '@nestjs/config';
     TutorModule,
     StudentModule,
     AuthModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
   ],
 })
 export class AppModule {}
