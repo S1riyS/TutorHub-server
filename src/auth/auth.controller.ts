f7670e252dbe5a5f9c98232111a48575ade03982
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2023-08-19 12:01:00 +0500

    feat(auth): refresh token is set to cookies

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,19 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Post, Res, UnauthorizedException } from '@nestjs/common';
 import { CreateUserDTO as RegisterDTO } from '@user/dto';
 import { LoginDTO } from './dto';
 import { AuthService } from './auth.service';
+import { Tokens } from './interfaces';
+import { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  private readonly REFRESH_TOKEN_COOKIE_NAME = 'REFRESH_TOKEN';
+
+  constructor(
+    private readonly config: ConfigService,
+    private readonly authService: AuthService,
+  ) {}
 
   @Post('register')
   register(@Body() dto: RegisterDTO) {
@@ -14,11 +22,24 @@ export class AuthController {
   }
 
   @Post('login')
-  async login(@Body() dto: LoginDTO) {
+  async login(@Body() dto: LoginDTO, @Res() response: Response) {
     const tokens = await this.authService.login(dto);
-    return { accessToken: tokens.accessToken };
+    this.setRefreshTokenToCookies(tokens, response);
   }
 
   @Get('refresh')
   refreshTokens() {}
+
+  private setRefreshTokenToCookies(tokens: Tokens, response: Response) {
+    if (!tokens) throw new UnauthorizedException('');
+
+    response.cookie(this.REFRESH_TOKEN_COOKIE_NAME, tokens.refreshToken.value, {
+      httpOnly: true,
+      sameSite: 'lax',
+      expires: new Date(tokens.refreshToken.exp),
+      secure: this.config.get('NODE_ENV') === 'production',
+      path: '/',
+    });
+    response.status(HttpStatus.CREATED).json({ accessToken: tokens.accessToken });
+  }
 }
