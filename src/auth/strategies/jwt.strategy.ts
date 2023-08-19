88c063d1fb9605f74dec51c8fb32aa6bbbeda084
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2023-08-19 15:26:57 +0500

    feat(auth): added `JwtStrategy`

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,26 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtPayload } from '../interfaces';
+import { UserService } from '@user/user.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private readonly config: ConfigService,
+    private readonly userService: UserService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: config.get('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: JwtPayload) {
+    const user = await this.userService.findOne(payload.id);
+    if (!user) throw new UnauthorizedException();
+    return payload;
+  }
+}
