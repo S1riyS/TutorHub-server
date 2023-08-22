0be0438a076809347d6d506d3b7705c5d4dd161a
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2023-08-22 14:31:09 +0500

    feat(app): added new `start:swagger-only` CLI command

--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,25 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import swaggerSetup from './swaggerSetup';
 import * as cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
+  // Creating app and retrieving config
   const app = await NestFactory.create(AppModule);
-  app.use(cookieParser());
-  app.useGlobalPipes(
-    new ValidationPipe({
-      transform: true,
-      whitelist: true,
-    }),
-  );
+  const config = app.get<ConfigService>(ConfigService);
 
-  await swaggerSetup(app);
+  // Setting up swagger
+  const isSwaggerOnly = config.get<boolean>('SWAGGER_ONLY', false);
+  await swaggerSetup(app, isSwaggerOnly);
 
-  await app.listen(3000);
+  // If .env variable "SWAGGER_ONLY" is true app stops, otherwise it starts
+  if (!isSwaggerOnly) {
+    app.use(cookieParser());
+    app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+
+    const PORT = config.get<number>('PORT');
+    await app.listen(PORT);
+  }
 }
 
 bootstrap();
