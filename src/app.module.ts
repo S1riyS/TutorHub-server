77978248af38d5faa0be1f19616c0ff26f82dc9d
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2023-08-22 22:49:51 +0500

    chore(app): imports use aliases from tsconfig

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from '@prisma/prisma.module';
 import { UserModule } from '@user/user.module';
-import { TutorModule } from './tutor/tutor.module';
-import { StudentModule } from './student/student.module';
-import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
-import { APP_GUARD } from '@nestjs/core';
-import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import { AuthModule } from '@auth/auth.module';
+import { JwtAuthGuard } from '@auth/guards/jwt-auth.guard';
+import { TutorModule } from '@tutor/tutor.module';
+import { StudentModule } from '@student/student.module';
 
 @Module({
   imports: [
