19da385e4207361c4f63680e62952089ede37a53
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2023-08-23 17:39:53 +0500

    feat(subject): added `SubjectEntity`

---
+++ src/subject/entities/subject.entity.ts
@@ -0,0 +1,21 @@
+import { Subject } from '@prisma/client';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+import { Transform } from 'class-transformer';
+
+export class SubjectEntity implements Subject {
+  @ApiProperty({ example: 'ab250e4f-30cc-4e25-aea5-63841a377aad' })
+  id: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Transform((param) => param.value.charAt(0).toUpperCase() + param.value.slice(1))
+  @ApiProperty({ example: 'Английский' })
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Transform((param) => param.value.toLowerCase())
+  @ApiProperty({ example: 'english' })
+  nameForURL: string;
+}
