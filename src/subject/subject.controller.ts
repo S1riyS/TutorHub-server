93ea389923dee0e6eb31b8ed6b785bf8b855a283
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2023-08-23 17:33:05 +0500

    feat(subject): added `getOne` and `getAll` endpoints

--- src/subject/subject.controller.ts
+++ src/subject/subject.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { SubjectService } from '@subject/subject.service';
 
-@Controller('subject')
-export class SubjectController {}
+@Controller('subjects')
+export class SubjectController {
+  constructor(private readonly subjectService: SubjectService) {}
+
+  @Get(':subjectId')
+  getOne(@Param('subjectId') subjectId: string) {
+    return this.subjectService.getOne(subjectId);
+  }
+
+  @Get()
+  getAll() {
+    return this.subjectService.getAll();
+  }
+
+  @Post()
+  add(dto) {}
+
+  @Patch(':subjectId')
+  update(@Param('subjectId') subjectId: string, dto) {}
+
+  @Delete(':subjectId')
+  delete(@Param('subjectId') subjectId: string) {}
+}
--- src/subject/subject.service.ts
+++ src/subject/subject.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from '@prisma/prisma.service';
+import { Subject, Topic } from '@prisma/client';
 
 @Injectable()
-export class SubjectService {}
+export class SubjectService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getOne(subjectId: string, throwWhenNotFound = false): Promise<Subject & { topics: Topic[] }> {
+    const subject = await this.prisma.subject.findUnique({
+      where: { id: subjectId },
+      include: { topics: true },
+    });
+
+    if (!subject) {
+      if (throwWhenNotFound) throw new NotFoundException('Subject not found');
+      return null;
+    }
+    return subject;
+  }
+
+  async getAll(): Promise<Subject[]> {
+    return this.prisma.subject.findMany();
+  }
+}
